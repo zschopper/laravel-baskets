21df5d5e5d582675af95be78ac01f0539c29cf7a
glitchy <zschopper@gmail.com>
2023-11-22 18:30:42 +0100

    routes, cost field for user, seeder

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductTypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+
+Route::apiResource('product-types', App\Http\Controllers\ProductTypeController::class);
+Route::apiResource('products', App\Http\Controllers\ProductController::class);
+Route::apiResource('baskets', App\Http\Controllers\BasketController::class);
